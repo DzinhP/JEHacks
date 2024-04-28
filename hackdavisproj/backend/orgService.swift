9f5bd0d8ba4f94a7a5edf9f109c6c6aab107cc6d
n6thnguyen <n6thnguyen@gmail.com>
2024-04-27 17:33:43 -0700

    finished data

--- hackdavisproj/backend/orgService.swift
+++ hackdavisproj/backend/orgService.swift
@@ -24,8 +24,6 @@ class OrgService {
         newOrganization.location = location
         newOrganization.contactInfo = contactInfo
         newOrganization.descriptionText = descriptionText
-        
-        
         do {
             try context.save()
             print("Organization saved successfully!")
