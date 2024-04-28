f6fc7ee13072064ca712d09401c4d683d8d73481
n6thnguyen <n6thnguyen@gmail.com>
2024-04-28 09:59:56 -0700

    final

--- hackdavisproj/NavBarContent/EventModel.swift
+++ hackdavisproj/NavBarContent/EventModel.swift
@@ -26,16 +26,45 @@ class EventData: ObservableObject {
 
     init() {
         var aggieHouseEvent = VolunteerEvent(
-                    title: "Aggie House",
-                    description: "Join us to help operate and maintain our shelter!",
-                    date: Date()
-                )
-                
-                // Load and set the image separately
-                aggieHouseEvent.image = UIImage(named: "AggieHouse")
-                
-                // Adding the event with an image
-                self.events.append(aggieHouseEvent)    }
+            title: "Aggie House",
+            description: "Join us to help operate and maintain our shelter!",
+            date: Date()
+        )
+        
+        // Load and set the image separately
+        aggieHouseEvent.image = UIImage(named: "AggieHouse")
+        
+        // Adding the event with an image
+        self.events.append(aggieHouseEvent)
+        
+        
+        
+        var arboretumGarden = VolunteerEvent(
+            title: "Arboretum and Public Garden",
+            description: "Learn new skill and support the work of UC Davis's living museum, the Arboretum!",
+            date: Date()
+        )
+        
+        // Load and set the image separately
+        arboretumGarden.image = UIImage(named: "ArboretumGarden")
+        
+        // Adding the event with an image
+        self.events.append(arboretumGarden)
+        
+        
+        
+        var lifeOfKai = VolunteerEvent(
+            title: "Life of Kai Animal Shelter",
+            description: "Help animals find their forever homes with Life of Kai!",
+            date: Date()
+        )
+        
+        // Load and set the image separately
+        lifeOfKai.image = UIImage(named: "LifeOfKai")
+        
+        // Adding the event with an image
+        self.events.append(lifeOfKai)
+    }
 
     func signUp(for event: VolunteerEvent) {
         if !isSignedUp(for: event) {
